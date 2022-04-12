d8d08dc22b05c64983fccf79020a099e44adc97a
Diego Henrique <diegohrp20@gmail.com>
2022-04-12 19:38:31 -0300

    Structuring the grid where each color will be exposed on the page.

---
+++ script.js
@@ -0,0 +1,23 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order){
+        
+    }
+}
